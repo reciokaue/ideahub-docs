a00171c27e366241ae0a13b286b4aa2f7dd47f01
reciokaue <kaue.recio2@gmail.com>
2025-05-22 16:54:58 -0300

    build fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 const isGithubPages = process.env.GITHUB_PAGES === 'true';
 
 const nextConfig = {
-  output: 'export',
   basePath: isGithubPages ? '/ideahub-docs' : '',
   images: {
     unoptimized: true,
--- utils/useThrottle.ts
+++ utils/useThrottle.ts
@@ -1,23 +1,23 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-interface useThrottleProps {
-  delay: number
-}
+type useThrottleProps = {
+  delay: number;
+};
 
-export function useThrottle({ delay }: useThrottleProps){
-  const [ timeOutId, setTimeoutId ] = useState()
+export function useThrottle({ delay }: useThrottleProps) {
+  const [timeOutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
 
   function throttle(fn: () => void) {
-    if(timeOutId)
-      clearTimeout(timeOutId)
+    if (timeOutId) clearTimeout(timeOutId);
 
     const id = setTimeout(() => {
       fn();
     }, delay);
-    setTimeoutId(id)
+
+    setTimeoutId(id);
   }
 
   return {
-    throttle
-  }
+    throttle,
+  };
 }
