6d5a508601aaad3902c3f52f9a25a980b606f06e
reciokaue <kaue.recio2@gmail.com>
2025-05-22 16:41:23 -0300

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
+const nextConfig = {
+  output: 'export',
+  basePath: isGithubPages ? '/ideahub-docs' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
